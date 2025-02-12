caf2ca02ad49637f1f71d7e5a595996f201d4e94
Akshayexai <akshayexai@gmail.com>
2025-02-12 21:24:04 +0530

    Added support for Conjured items and updated test cases

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,3 +1,54 @@
+# # -*- coding: utf-8 -*-
+
+# class GildedRose(object):
+
+#     def __init__(self, items):
+#         self.items = items
+
+#     def update_quality(self):
+#         for item in self.items:
+#             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+#                 if item.quality > 0:
+#                     if item.name != "Sulfuras, Hand of Ragnaros":
+#                         item.quality = item.quality - 1
+#             else:
+#                 if item.quality < 50:
+#                     item.quality = item.quality + 1
+#                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
+#                         if item.sell_in < 11:
+#                             if item.quality < 50:
+#                                 item.quality = item.quality + 1
+#                         if item.sell_in < 6:
+#                             if item.quality < 50:
+#                                 item.quality = item.quality + 1
+#             if item.name != "Sulfuras, Hand of Ragnaros":
+#                 item.sell_in = item.sell_in - 1
+#             if item.sell_in < 0:
+#                 if item.name != "Aged Brie":
+#                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
+#                         if item.quality > 0:
+#                             if item.name != "Sulfuras, Hand of Ragnaros":
+#                                 item.quality = item.quality - 1
+#                     else:
+#                         item.quality = item.quality - item.quality
+#                 else:
+#                     if item.quality < 50:
+#                         item.quality = item.quality + 1
+
+
+# class Item:
+#     def __init__(self, name, sell_in, quality):
+#         self.name = name
+#         self.sell_in = sell_in
+#         self.quality = quality
+
+#     def __repr__(self):
+#         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+# ================================================================================================================
+
+
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
@@ -7,33 +58,40 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            if item.name.startswith("Conjured"):
+                degrade_amount = 2
+            else:
+                degrade_amount = 1
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality -= degrade_amount
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality += 1
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality += 1
+
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
+
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality -= degrade_amount
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,3 +1,23 @@
+# # -*- coding: utf-8 -*-
+# import unittest
+
+# from gilded_rose import Item, GildedRose
+
+
+# class GildedRoseTest(unittest.TestCase):
+#     def test_foo(self):
+#         items = [Item("foo", 0, 0)]
+#         gilded_rose = GildedRose(items)
+#         gilded_rose.update_quality()
+#         self.assertEqual("fixme", items[0].name)
+
+        
+# if __name__ == '__main__':
+#     unittest.main()
+
+# ===========================================================================================================
+
+
 # -*- coding: utf-8 -*-
 import unittest
 
@@ -5,12 +25,28 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_regular_item(self):
+        items = [Item("foo", 10, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(19, items[0].quality)
 
-        
+    def test_conjured_item(self):
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].quality)  # Degrades by 2 instead of 1
+        self.assertEqual(2, items[0].sell_in)
+
+    def test_conjured_item_past_sell_date(self):
+        items = [Item("Conjured Mana Cake", 0, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(2, items[0].quality)  # Degrades by 4 instead of 2
+        self.assertEqual(-1, items[0].sell_in)
+
+
 if __name__ == '__main__':
     unittest.main()
